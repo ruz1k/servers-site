560982ae677c551abd7c923be1dd8cdad5c5e37b
ruz1k <outpac1488@yandex.ru>
2022-03-25 20:47:17 +0300

    WIP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import '../styles/_global.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-    <Component {...pageProps} />
+    <Head>
+        <title>Аренда сервера для удаленной работы</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <Component {...pageProps} />
+    </Head>
 );
 
 export default MyApp;
